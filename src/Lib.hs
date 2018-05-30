fbd7ab75c242b67f604dd124f1295479ea3b54f0
Alec Reeves <areeves@cj.com>
2018-05-30 13:29:55 -0700

    chater 3.8 exercises question 1

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,4 +3,13 @@ module Lib
     ) where
 
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do
+    putStrLn "\nChapter 3.8 -- Chapter Exercises"
+    print $ concat [[1,2,3], [4,5,6]]
+    print $  (++) [1, 2, 3] [4, 5, 6]
+    print $ (++) "hello" " world"
+    print $ ["hello" ++ " world"]
+    print $ "hello" !! 4
+    print $ (!!) "hello" 4
+    print $ take 4 " lovely"
+    print $ take 3 "awesome"
